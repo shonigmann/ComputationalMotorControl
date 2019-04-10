f633cd3999573d9c6fd9e689f4211d03e8b0b0a6
kiliamk <kiliamk@gmail.com>
2019-04-10 20:22:04 +0200

    lab5 and lab6

--- Lab6/Python/exercise3.py
+++ Lab6/Python/exercise3.py
@@ -30,6 +30,11 @@ plt.rc('ytick', labelsize=14.0)    # fontsize of the tick labels
 
 
 def exercise3():
+#    exercise3a()
+    exercise3b()
+
+
+def exercise3a():
     """ Main function to run for Exercise 3.
 
     Parameters
@@ -80,14 +85,14 @@ def exercise3():
     ##### Neural Network #####
     # The network consists of four neurons
     N_params = NetworkParameters()  # Instantiate default network parameters
-    N_params.D = 1.  # To change a network parameter
+    N_params.D = 2.  # To change a network parameter
     # Similarly to change w -> N_params.w = (4x4) array
     print(N_params.w)
     ############################# Exercise 3A  ######################
-    N_params.w = np.asarray([[ 0, -1,  1, -1],
-                             [-1,  0, -1,  1],
-                             [-1,  0,  0,  0], 
-                             [ 0, -1,  0,  0]])*5
+    N_params.w = np.transpose(np.asarray([[ 0, -1,  1, -1],
+                                          [-1,  0, -1,  1],
+                                          [-1,  0,  0,  0], 
+                                          [ 0, -1,  0,  0]]))*5
     print(N_params.w, N_params.D, N_params.tau, N_params.b, N_params.exp )
 
     
@@ -106,7 +111,7 @@ def exercise3():
     sys.add_neural_system(neural_network)
 
     ##### Time #####
-    t_max = 4.5  # Maximum simulation time
+    t_max = 2.  # Maximum simulation time
     time = np.arange(0., t_max, 0.001)  # Time vector
 
     ##### Model Initial Conditions #####
@@ -130,20 +135,19 @@ def exercise3():
 
 #    sim.add_external_inputs_to_network(np.ones((len(time), 4)))
     
-    wave_h1 = np.sin(time*3)*2               #makes a sinusoidal wave from 'time'
-    wave_h2 = np.sin(time*3 + np.pi)*1       #makes a sinusoidal wave from 'time'
+#    wave_h1 = np.sin(time*3)*2               #makes a sinusoidal wave from 'time'
+#    wave_h2 = np.sin(time*3 + np.pi)*1       #makes a sinusoidal wave from 'time'
+#    
+#    wave_h1[wave_h1<0] = 0      #formality of passing negative values to zero
+#    wave_h2[wave_h2<0] = 0      #formality of passing negative values to zero
+#    
+#    act1 = wave_h1.reshape(len(time), 1) #makes a vertical array like act1
+#    act2 = wave_h2.reshape(len(time), 1) #makes a vertical array like act1
+#    column = np.ones((len(time), 1))
     
-    wave_h1[wave_h1<0] = 0      #formality of passing negative values to zero
-    wave_h2[wave_h2<0] = 0      #formality of passing negative values to zero
+#    ext_in = np.hstack((act1, column, act2, column))
     
-    act1 = wave_h1.reshape(len(time), 1) #makes a vertical array like act1
-    act2 = wave_h2.reshape(len(time), 1) #makes a vertical array like act1
-    column = np.ones((len(time), 1))
-    
-    ext_in = np.hstack((act1, column, act2, column))
-    
-    sim.add_external_inputs_to_network(ext_in)
-
+#    sim.add_external_inputs_to_network(ext_in)
     sim.initalize_system(x0, time)  # Initialize the system state
 
     # Integrate the system for the above initialized state and time
@@ -164,12 +168,248 @@ def exercise3():
     muscle1_results = sim.sys.muscle_sys.Muscle1.results
     muscle2_results = sim.sys.muscle_sys.Muscle2.results
 
-    # Plotting the results
-    plt.figure('Pendulum')
+    # Plotting the results: Position(phase) vs time
+    plt.figure('Pendulum Phase')
     plt.title('Pendulum Phase')
-    plt.plot(res[:, 0], res[:, :2])
+    plt.plot(res[:, 0], res[:,1])   #to plot pendulum Position (phase)
+#    plt.plot(res[:, 0], time)   #to plot position
+#    plt.plot(res[:, 0], res[:, -5:-1])  # to Plot neurons' states
+    plt.xlabel('time [s]')
+    plt.ylabel('Position [rad]')
+    plt.grid()
+
+    # Plotting the results: Velocity vs Position (phase)
+    plt.figure('Pendulum Vel v.s. Phase')
+    plt.title('Pendulum Vel v.s. Phase')
+    plt.plot(res[:, 1], res[:,2])   #to plot Velocity vs Position (phase)
     plt.xlabel('Position [rad]')
     plt.ylabel('Velocity [rad.s]')
+    plt.grid()
+
+    # Plotting the results: Velocity vs time 
+    plt.figure('Pendulum Velocity')
+    plt.title('Pendulum Velocity')
+    plt.plot(res[:, 0], res[:,2])   #to plot Velocity vs Position
+    plt.xlabel('time [s]')
+    plt.ylabel('Velocity [rad.s]')
+    plt.grid()
+
+    # Plotting the results: Output of the network
+    plt.figure('Network output')
+    plt.title('Network output')
+    plt.plot(res[:, 0], res[:,-5:-1], label='neuron1')   #to plot Velocity vs Position
+    plt.xlabel('time [s]')
+    plt.ylabel('Stimulation ')
+    plt.legend(loc='upper left')
+    plt.grid()
+    
+
+
+    if DEFAULT["save_figures"] is False:
+        plt.show()
+    else:
+        figures = plt.get_figlabels()
+        pylog.debug("Saving figures:\n{}".format(figures))
+        for fig in figures:
+            plt.figure(fig)
+            save_figure(fig)
+            plt.close(fig)
+
+    # To animate the model, use the SystemAnimation class
+    # Pass the res(states) and systems you wish to animate
+    simulation = SystemAnimation(
+        res,
+        sim.sys.pendulum_sys,
+        sim.sys.muscle_sys,
+        sim.sys.neural_sys)
+    # To start the animation
+    simulation.animate()
+
+def exercise3b():
+    """ Main function to run for Exercise 3.
+
+    Parameters
+    ----------
+        None
+
+    Returns
+    -------
+        None
+    """
+    # Define and Setup your pendulum model here
+    # Check Pendulum.py for more details on Pendulum class
+    P_params = PendulumParameters()  # Instantiate pendulum parameters
+    P_params.L = 0.5  # To change the default length of the pendulum
+    P_params.m = 1.  # To change the default mass of the pendulum
+    pendulum = PendulumSystem(P_params)  # Instantiate Pendulum object
+
+    #### CHECK OUT Pendulum.py to ADD PERTURBATIONS TO THE MODEL #####
+
+    pylog.info('Pendulum model initialized \n {}'.format(
+        pendulum.parameters.showParameters()))
+
+    # Define and Setup your pendulum model here
+    # Check MuscleSytem.py for more details on MuscleSytem class
+    M1_param = MuscleParameters()  # Instantiate Muscle 1 parameters
+    M1_param.f_max = 1500  # To change Muscle 1 max force
+    M2_param = MuscleParameters()  # Instantiate Muscle 2 parameters
+    M2_param.f_max = 1500  # To change Muscle 2 max force
+    M1 = Muscle(M1_param)  # Instantiate Muscle 1 object
+    M2 = Muscle(M2_param)  # Instantiate Muscle 2 object
+    # Use the MuscleSystem Class to define your muscles in the system
+    muscles = MuscleSytem(M1, M2)  # Instantiate Muscle System with two muscles
+    pylog.info('Muscle system initialized \n {} \n {}'.format(
+        M1.parameters.showParameters(),
+        M2.parameters.showParameters()))
+
+    # Define Muscle Attachment points
+    m1_origin = np.array([-0.17, 0.0])  # Origin of Muscle 1
+    m1_insertion = np.array([0.0, -0.17])  # Insertion of Muscle 1
+
+    m2_origin = np.array([0.17, 0.0])  # Origin of Muscle 2
+    m2_insertion = np.array([0.0, -0.17])  # Insertion of Muscle 2
+
+    # Attach the muscles
+    muscles.attach(np.array([m1_origin, m1_insertion]),
+                   np.array([m2_origin, m2_insertion]))
+
+    ##### Neural Network #####
+    # The network consists of four neurons
+    N_params = NetworkParameters()  # Instantiate default network parameters
+    N_params.D = 2.  # To change a network parameter
+    # Similarly to change w -> N_params.w = (4x4) array
+    print(N_params.w)
+    ############################# Exercise 3A  ######################
+    N_params.w = np.transpose(np.asarray([[ 0, -1,  1, -1],
+                                          [-1,  0, -1,  1],
+                                          [-1,  0,  0,  0], 
+                                          [ 0, -1,  0,  0]]))*5
+    print(N_params.w, N_params.D, N_params.tau, N_params.b, N_params.exp )
+
+    
+
+    # Create a new neural network with above parameters
+    neural_network = NeuralSystem(N_params)
+    pylog.info('Neural system initialized \n {}'.format(
+        N_params.showParameters()))
+
+    # Create system of Pendulum, Muscles and neural network using SystemClass
+    # Check System.py for more details on System class
+    sys = System()  # Instantiate a new system
+    sys.add_pendulum_system(pendulum)  # Add the pendulum model to the system
+    sys.add_muscle_system(muscles)  # Add the muscle model to the system
+    # Add the neural network to the system
+    sys.add_neural_system(neural_network)
+
+    ##### Time #####
+    t_max = 2.  # Maximum simulation time
+    time = np.arange(0., t_max, 0.001)  # Time vector
+
+    ##### Model Initial Conditions #####
+    x0_P = np.array([0., 0.])  # Pendulum initial condition
+
+    # Muscle Model initial condition
+    x0_M = np.array([0., M1.L_OPT, 0., M2.L_OPT])
+
+    x0_N = np.array([-0.5, 1, 0.5, 1])  # Neural Network Initial Conditions
+
+    x0 = np.concatenate((x0_P, x0_M, x0_N))  # System initial conditions
+
+    ##### System Simulation #####
+    # For more details on System Simulation check SystemSimulation.py
+    # SystemSimulation is used to initialize the system and integrate
+    # over time
+
+    sim = SystemSimulation(sys)  # Instantiate Simulation object
+
+    # Add external inputs to neural network
+
+#    sim.add_external_inputs_to_network(np.ones((len(time), 4)))
+    
+#    wave_h1 = np.sin(time*3)*2               #makes a sinusoidal wave from 'time'
+#    wave_h2 = np.sin(time*3 + np.pi)*1       #makes a sinusoidal wave from 'time'
+#    
+#    wave_h1[wave_h1<0] = 0      #formality of passing negative values to zero
+#    wave_h2[wave_h2<0] = 0      #formality of passing negative values to zero
+#    
+#    act1 = wave_h1.reshape(len(time), 1) #makes a vertical array like act1
+#    act2 = wave_h2.reshape(len(time), 1) #makes a vertical array like act1
+    print (type(float(len(time))))
+    l = (len(time))
+    l = int(l/2.0)
+    print(l)
+    column = np.ones((l, 1))*0
+    column = np.append(np.ones((l, 1)))
+    print (column)
+    print (type(len(time)))
+    
+
+#    ext_in = np.hstack((act1, column, act2, column))    
+    ext_in = np.hstack((column, column, column, column))
+    
+    sim.add_external_inputs_to_network(ext_in)
+    sim.initalize_system(x0, time)  # Initialize the system state
+
+    # Integrate the system for the above initialized state and time
+    sim.simulate()
+
+    # Obtain the states of the system after integration
+    # res is np.array [time, states]
+    # states vector is in the same order as x0
+    res = sim.results()
+
+    # Obtain the states of the system after integration
+    # res is np.array [time, states]
+    # states vector is in the same order as x0
+    res = sim.results()
+
+    # In order to obtain internal states of the muscle
+    # you can access the results attribute in the muscle class
+    muscle1_results = sim.sys.muscle_sys.Muscle1.results
+    muscle2_results = sim.sys.muscle_sys.Muscle2.results
+
+     # Plotting the results: Position(phase) vs time
+    plt.figure('Pendulum Phase & Stimulation')
+    plt.title('Pendulum Phase & Stimulation')
+    plt.plot(res[:, 0], res[:,1])   #to plot pendulum Position (phase)
+#    plt.plot(res[:, 0], time)   #to plot position
+#    plt.plot(res[:, 0], res[:, -5:-1])  # to Plot neurons' states
+    plt.xlabel('time [s]')
+    plt.ylabel('Position [rad]')
+    plt.grid()
+
+    # Plotting the results: Velocity vs Position (phase)
+    plt.figure('Pendulum Vel v.s. Phase & Stimulation')
+    plt.title('Pendulum Vel v.s. Phase & Stimulation')
+    plt.plot(res[:, 1], res[:,2])   #to plot Velocity vs Position (phase)
+    plt.xlabel('Position [rad]')
+    plt.ylabel('Velocity [rad.s]')
+    plt.grid()
+
+    # Plotting the results: Velocity vs time 
+    plt.figure('Pendulum Velocity & Stimulation')
+    plt.title('Pendulum Velocity & Stimulation')
+    plt.plot(res[:, 0], res[:,2])   #to plot Velocity vs Position
+    plt.xlabel('time [s]')
+    plt.ylabel('Velocity [rad.s]')
+    plt.grid()
+
+    # Plotting the results: Output of the network
+    plt.figure('Network output & Stimulation')
+    plt.title('Network output & Stimulation')
+    plt.plot(res[:, 0], res[:,-5:-1], label='neuron1')   #to plot Velocity vs Position
+    plt.xlabel('time [s]')
+    plt.ylabel('Stimulation ')
+    plt.legend(loc='upper left')
+    plt.grid()
+    
+    # Plotting the results: Output of the network
+    plt.figure('Network output & Stimulation')
+    plt.title('Network output & Stimulation')
+    plt.plot(res[:, 0], res[:,-5:-1], label='neuron1')   #to plot Velocity vs Position
+    plt.xlabel('time [s]')
+    plt.ylabel('Stimulation ')
+    plt.legend(loc='upper left')
     plt.grid()
 
     if DEFAULT["save_figures"] is False:
@@ -191,6 +431,7 @@ def exercise3():
         sim.sys.neural_sys)
     # To start the animation
     simulation.animate()
+
 
 
 if __name__ == '__main__':
