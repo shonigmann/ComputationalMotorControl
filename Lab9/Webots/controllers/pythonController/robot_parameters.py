76e9f25f9082f9d2a7c86a2519424ca4dd1f67ed
Louis Munier <louis.munier@epfl.ch>
2019-05-09 16:37:55 +0200

    Move parameters and fill exercise_9b.py file.

--- Lab9/Webots/controllers/pythonController/robot_parameters.py
+++ Lab9/Webots/controllers/pythonController/robot_parameters.py
@@ -41,15 +41,39 @@ class RobotParameters(dict):
 
     def set_frequencies(self, parameters):
         """Set frequencies"""
-        self.freqs = parameters.freqs
+        frequency = parameters.freqs
+        self.freqs = frequency * np.ones(2 * self.n_body_joints + self.n_legs_joints)
 
     def set_coupling_weights(self, parameters):
         """Set coupling weights"""
-        self.coupling_weights = parameters.coupling_weights
+        weight = parameters.coupling_weights
+
+        for i in range(self.n_body_joints):
+            if i != self.n_body_joints-1:
+                self.coupling_weights[i, i + 1] = weight
+                self.coupling_weights[i + self.n_body_joints, i + self.n_body_joints + 1] = weight
+
+                self.coupling_weights[i + 1, i] = weight
+                self.coupling_weights[i + self.n_body_joints + 1, i + self.n_body_joints] = weight
+
+            self.coupling_weights[i, i + self.n_body_joints] = weight
+            self.coupling_weights[i + self.n_body_joints, i] = weight
 
     def set_phase_bias(self, parameters):
         """Set phase bias"""
-        self.phase_bias = parameters.phase_bias
+        body_bias = parameters.body_phase_bias
+        limb_bias = parameters.limb_phase_bias
+
+        for i in range(self.n_body_joints):
+            if i != self.n_body_joints-1:
+                self.phase_bias[i, i + 1] = -body_bias
+                self.phase_bias[i + self.n_body_joints, i + self.n_body_joints + 1] = -body_bias
+
+                self.phase_bias[i + 1, i] = body_bias
+                self.phase_bias[i + self.n_body_joints + 1, i + self.n_body_joints] = body_bias
+
+            self.phase_bias[i, i + self.n_body_joints] = limb_bias
+            self.phase_bias[i + self.n_body_joints, i] = limb_bias
 
     def set_amplitudes_rate(self, parameters):
         """Set amplitude rates"""
@@ -57,5 +81,6 @@ class RobotParameters(dict):
 
     def set_nominal_amplitudes(self, parameters):
         """Set nominal amplitudes"""
-        self.nominal_amplitudes = parameters.nominal_amplitudes
+        amplitude = parameters.nominal_amplitudes
+        self.nominal_amplitudes = amplitude * np.ones(2 * self.n_body_joints + self.n_legs_joints)
 
